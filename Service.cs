4f9fc15e66dd3ab7e636e17fa6259f858de91c87
Olua12-Bit <verklieva@gmail.com>
2024-06-27 00:17:00 +0500

    Create Service.cs

---
+++ Service.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiaryOfaNailMaster
+{
+    internal class Service
+    {
+        public int IdService { get; set; }
+        public string NameService { get; set; }
+        public decimal PriceService { get; set; }
+
+    }
+}
